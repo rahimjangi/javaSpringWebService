d205309475ca9279e9a353183fbc172ed0531401
pc <rahimjangi1982@gmail.com>
2022-08-17 20:41:31 +0200

    Initial Init;

---
+++ src/main/java/com/raiseup/javaSpringWebService/JavaSpringWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raiseup.javaSpringWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raiseup/javaSpringWebService/JavaSpringWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiseup.javaSpringWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
